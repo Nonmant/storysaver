cfdc281c34e7662a6365d9c0a1f98d80ba5d325f
Anton Bolotskih <89161424838@yandex.ru>
2021-09-28 23:34:32 +0300

    Added ability to parse grouped stories Extended logs

--- background.js
+++ background.js
@@ -14,31 +14,31 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 function sendBackground(tabId) {
-  //alert("sendBackground sending to tab "+tabId);
+  console.log('[storysaver]:',"sendBackground sending to tab "+tabId);
   //chrome.tabs.sendMessage(tab.id, {from:'background savestories'}, function(response) {alert(response.name);});
   chrome.tabs.sendMessage(tabId, { from: 'background savestories' });
 }
 
 function backgroundListen(message) {
-  //alert('background listener');
+  console.log('[storysaver]:','background listener');
   if (message.from)
     switch (message.from) {
       case 'content savestories':
-        //alert('message from content');
+        console.log('[storysaver]:','message from content');
         if (message.popupBody != 'none') {
-          //alert('background received non-null popupBody');
+          console.log('[storysaver]:','background received non-null popupBody');
           //alert(message.popupBody);
           var views = chrome.extension.getViews({ type: 'popup' });
           for (var i in views) {
             views[i].document.body.innerHTML = message.popupBody;
           }
         } else {
-          //alert('no message.popupBody!');
+          console.log('[storysaver]:','no message.popupBody!');
         }
         break;
 
       case 'popup savestories':
-        //alert('message from popup');
+        console.log('[storysaver]:','message from popup');
         if (message.tabId) sendBackground(message.tabId);
         break;
       default:
--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 var makeBody=function(){
-  //alert('document: script run');
+  console.log('[storysaver]:','document: script run');
   var stories=[], count=0;
 
   for(var i in cur){
@@ -15,44 +15,59 @@ var makeBody=function(){
     settings.className='settings';
     settings.title='Настройки';
     popupBody.appendChild(settings);
-    for(var i in stories){
-      var story=stories[i];
-      var aPerson=document.createElement('button');
-      aPerson.id='';
-      var dPerson=document.createElement('div');
-      aPerson.title='Сохранить все истории пользователя';
-
-      var aPersonImage=story.author.photo.indexOf('http')!=-1?story.author.photo:'https://vk.com/images/icons/msg_error.png';
-      aPerson.setAttribute('style', 'background-image: url(' + aPersonImage + ');');
-
-      var pPerson=document.createElement('span');
-      pPerson.innerText=story.author.name.split(' ')[0];
-      aPerson.appendChild(pPerson);
-      dPerson.appendChild(document.createElement('hr'));
-
-      for(var j in story.items){
-        var item=story.items[j];
-        var aItem=document.createElement('button');
-        var sItem=document.createElement('span');
-          if(item.type==`photo`){
-            aItem.id=item.photo_url;
-            sItem.className='photo';
-            sItem.setAttribute('style', 'background-image:url('+item.photo_url+');');
-          }
-          else {
-            aItem.id=item.video_url;
-            sItem.className='video';
-            sItem.setAttribute('style', 'background-image:url('+item.first_frame+');');
-          }
-        aPerson.id+=aItem.id+',';
-        aItem.className='story';
-        aItem.setAttribute('style', 'background-image: url(' + item.preview_url + ');');
-        aItem.appendChild(sItem);
-        dPerson.appendChild(aItem);
+    for(var i=0; i<stories.length; ++i){
+      try {
+        if(stories[i]?.grouped){
+          var groupedStories = stories[i].grouped;
+          stories.splice(i);
+          stories = stories.concat(groupedStories);
+          --i;
+        }
+      }catch (e) {
+        console.error('[storysaver]:',e, stories[i]);
       }
-      dPerson.insertBefore(aPerson, dPerson.firstChild);
-      popupBody.appendChild(dPerson);
-      popupBody.appendChild(document.createElement('p'));
+    }
+    for(var i in stories){
+      try {
+        var story = stories[i];
+        var aPerson = document.createElement('button');
+        aPerson.id = '';
+        var dPerson = document.createElement('div');
+        aPerson.title = 'Сохранить все истории пользователя';
+
+        var aPersonImage = story.author.photo.indexOf('http') != -1 ? story.author.photo : 'https://vk.com/images/icons/msg_error.png';
+        aPerson.setAttribute('style', 'background-image: url(' + aPersonImage + ');');
+
+        var pPerson = document.createElement('span');
+        pPerson.innerText = story.author.name.split(' ')[0];
+        aPerson.appendChild(pPerson);
+        dPerson.appendChild(document.createElement('hr'));
+
+        for (var j in story.items) {
+          var item = story.items[j];
+          var aItem = document.createElement('button');
+          var sItem = document.createElement('span');
+          if (item.type == `photo`) {
+            aItem.id = item.photo_url;
+            sItem.className = 'photo';
+            sItem.setAttribute('style', 'background-image:url(' + item.photo_url + ');');
+          } else {
+            aItem.id = item.video_url;
+            sItem.className = 'video';
+            sItem.setAttribute('style', 'background-image:url(' + item.first_frame + ');');
+          }
+          aPerson.id += aItem.id + ',';
+          aItem.className = 'story';
+          aItem.setAttribute('style', 'background-image: url(' + item.preview_url + ');');
+          aItem.appendChild(sItem);
+          dPerson.appendChild(aItem);
+        }
+        dPerson.insertBefore(aPerson, dPerson.firstChild);
+        popupBody.appendChild(dPerson);
+        popupBody.appendChild(document.createElement('p'));
+      } catch (e) {
+        console.error('[storysaver]:',e, stories[i]);
+      }
     }
     var aHome=document.createElement('a');
     aHome.href='https://vk.com/savestories';
@@ -69,9 +84,9 @@ var makeBody=function(){
 
 
 function contentListen(request, sender){
-  //alert('received message in content listener');
+  console.log('[storysaver]:','received message in content listener');
   if(request.from=='background savestories'){
-    //alert('content listener: from background');
+    console.log('[storysaver]:','content listener: from background');
     //sendResponse({"name":"Петя"});
     var script=document.createElement('script');
     script.type='text/javascript';
@@ -86,9 +101,9 @@ chrome.runtime.onMessage.addListener(contentListen);
 
 window.addEventListener("message", function(event) {
   if (event.source == window) {
-    //alert('received message in content event listener');
+    console.log('[storysaver]:','received message in content event listener');
     if(event.data.from=='document savestories'){
-      //alert("content event listener: from document");
+      console.log('[storysaver]:',"content event listener: from document");
       chrome.runtime.sendMessage({from:'content savestories', popupBody:event.data.popupBody, links:event.data.links});
     }
   }
